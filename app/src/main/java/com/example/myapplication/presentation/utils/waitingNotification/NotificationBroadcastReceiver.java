34c940660b2194c34ee911e26ca11d98bcab2ace
Shadow Berserker <iuliaglad07@gmail.com>
2024-02-17 18:29:04 +0300

    new changes

--- app/src/main/java/com/example/myapplication/presentation/utils/waitingNotification/NotificationBroadcastReceiver.java
+++ app/src/main/java/com/example/myapplication/presentation/utils/waitingNotification/NotificationBroadcastReceiver.java
@@ -1,11 +1,16 @@
 package com.example.myapplication.presentation.utils.waitingNotification;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 
-public class NotificationBroadcastReceiver extends android.content.BroadcastReceiver {
+public class NotificationBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-
+        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+            Intent serviceIntent = new Intent(context, NotificationForegroundService.class);
+            context.startService(serviceIntent);
+        }
     }
 }
