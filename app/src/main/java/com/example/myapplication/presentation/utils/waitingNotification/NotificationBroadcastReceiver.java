f7e4176dd63ac715da0e394330d1705806419244
Shadow Berserker <iuliaglad07@gmail.com>
2024-02-17 17:34:24 +0300

    new changes

---
+++ app/src/main/java/com/example/myapplication/presentation/utils/waitingNotification/NotificationBroadcastReceiver.java
@@ -0,0 +1,11 @@
+package com.example.myapplication.presentation.utils.waitingNotification;
+
+import android.content.Context;
+import android.content.Intent;
+
+public class NotificationBroadcastReceiver extends android.content.BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
